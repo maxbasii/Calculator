965a74d2fa2a75737936058c0b54c8dd2407d765
MaxBosiak <maxbosiak@gmail.com>
2021-02-01 13:36:12 +0200

    Calculate

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
-console.log("Hello world !!!")
-alert("Hi")
+function Calculate(){
+    let vall_1 = Number(document.getElementById("input1").value);
+    let vall_2 = Number(document.getElementById("input2").value);
+    let summ = vall_1 + vall_2;
+    document.getElementById("result").innerHTML = "Результат = " + summ;
+}
+function Minus(){
+    let vall_1 = Number(document.getElementById("input1").value);
+    let vall_2 = Number(document.getElementById("input2").value);
+    let summ = vall_1 - vall_2;
+    document.getElementById("result").innerHTML = "Результат = " + summ;
+}
+function Divide(){
+    let vall_1 = Number(document.getElementById("input1").value);
+    let vall_2 = Number(document.getElementById("input2").value);
+    let summ = vall_1 / vall_2;
+    document.getElementById("result").innerHTML = "Результат = " + summ;
+}
+function Multiplaid(){
+    let vall_1 = Number(document.getElementById("input1").value);
+    let vall_2 = Number(document.getElementById("input2").value);
+    let summ = vall_1 * vall_2;
+    document.getElementById("result").innerHTML = "Результат = " + summ;
+}
