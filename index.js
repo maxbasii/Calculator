c55203fb2c4971121bc2603c2722e3b6094f8006
MaxBosiak <maxbosiak@gmail.com>
2021-02-02 14:21:08 +0200

    correction of defects with zero

--- index.js
+++ index.js
@@ -2,23 +2,30 @@ function Calculate(){
     let vall_1 = Number(document.getElementById("input1").value);
     let vall_2 = Number(document.getElementById("input2").value);
     let summ = vall_1 + vall_2;
-    document.getElementById("result").innerHTML = "Результат = " + summ;
+    
+    document.getElementById("result").innerHTML = "Результат = " + ( +summ.toFixed(2) );
 }
 function Minus(){
     let vall_1 = Number(document.getElementById("input1").value);
     let vall_2 = Number(document.getElementById("input2").value);
     let summ = vall_1 - vall_2;
-    document.getElementById("result").innerHTML = "Результат = " + summ;
+    document.getElementById("result").innerHTML = "Результат = " + ( +summ.toFixed(2) );
 }
 function Divide(){
     let vall_1 = Number(document.getElementById("input1").value);
     let vall_2 = Number(document.getElementById("input2").value);
-    let summ = vall_1 / vall_2;
-    document.getElementById("result").innerHTML = "Результат = " + summ;
+    let summ;
+    if(vall_2 == "0"){
+       summ = vall_1
+    }else{
+        summ = vall_1 / vall_2;
+    }
+     
+    document.getElementById("result").innerHTML = "Результат = " + ( +summ.toFixed(2) );
 }
 function Multiplaid(){
     let vall_1 = Number(document.getElementById("input1").value);
     let vall_2 = Number(document.getElementById("input2").value);
     let summ = vall_1 * vall_2;
-    document.getElementById("result").innerHTML = "Результат = " + summ;
+    document.getElementById("result").innerHTML = "Результат = " + ( +summ.toFixed(2) );
 }
